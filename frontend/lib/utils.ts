41c47c2f4eb8bf5f0548b817e69c6a0092106087
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-03-07 08:36:39 +0300

    utils: add Stacks address validation helper

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -2,3 +2,7 @@
 export function objectPick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   return keys.reduce((acc, k) => { if (k in obj) acc[k] = obj[k]; return acc; }, {} as Pick<T, K>);
 }
+
+export function isValidStacksAddress(addr: string): boolean {
+  return /^S[PM][A-Z0-9]{38,}$/.test(addr);
+}
