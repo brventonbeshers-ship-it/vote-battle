eced2eb06ee1412ef7e6d8ea1cbe93ae06a5970c
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-03-07 08:36:40 +0300

    utils: add chunk array utility

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -6,3 +6,9 @@ export function objectPick<T extends object, K extends keyof T>(obj: T, keys: K[
 export function isValidStacksAddress(addr: string): boolean {
   return /^S[PM][A-Z0-9]{38,}$/.test(addr);
 }
+
+export function chunk<T>(arr: T[], size: number): T[][] {
+  const out: T[][] = [];
+  for (let i = 0; i < arr.length; i += size) out.push(arr.slice(i, i + size));
+  return out;
+}
