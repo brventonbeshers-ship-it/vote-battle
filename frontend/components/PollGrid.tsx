3ddd3f439577e9375f16858f388451172f2c58fc
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-03-22 13:00:06 +0300

    style: adjust poll grid gap spacing

--- frontend/components/PollGrid.tsx
+++ frontend/components/PollGrid.tsx
@@ -17,7 +17,7 @@ export default function PollGrid({ polls }: PollGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {polls.map((poll) => (
         <PollCard key={poll.id} poll={poll} />
       ))}
