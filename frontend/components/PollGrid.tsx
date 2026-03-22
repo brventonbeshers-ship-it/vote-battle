bd8b6e421bbcba7ceefc173a6568a153873c876e
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-03-22 09:58:11 +0300

    layout: tweak grid column configuration

--- frontend/components/PollGrid.tsx
+++ frontend/components/PollGrid.tsx
@@ -17,7 +17,7 @@ export default function PollGrid({ polls }: PollGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {polls.map((poll) => (
         <PollCard key={poll.id} poll={poll} />
       ))}
